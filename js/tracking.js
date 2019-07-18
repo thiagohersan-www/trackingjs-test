199ea80177246682a56584cfe17764f8131bc6ca
Thiago Hersan <thiago.hersan+github@gmail.com>
2019-07-18 13:43:09 -0300

    adds adapter.js

--- js/tracking.js
+++ js/tracking.js
@@ -249,8 +249,8 @@
     var resizeCanvas_ = function() {
       width = element.offsetWidth;
       height = element.offsetHeight;
-      canvas.width = width / 2;
-      canvas.height = height / 2;
+      canvas.width = 0.666 * width;
+      canvas.height = 0.666 * height;
     };
     resizeCanvas_();
     element.addEventListener('resize', resizeCanvas_);
